ecd8e8e12a1eb0d72779bafb5ffa3a32f8f08c26
rizavapy <rizava.ankimaka@gmail.com>
2025-07-18 12:54:22 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -292,6 +292,11 @@ elif menu == "Cara Perhitungan Manual":
                 st.warning("⚠️ Akurasi rendah (>5%)")
 # ===  FAKTOR KESALAHAN PENGUKURAN   === #
 elif menu == "Faktor Kesalahan":  
+      st.markdown("""
+    <p style='text-align: justify; font-size: 16px;'>
+    Dalam kegiatan pengukuran, khususnya dalam eksperimen fisika atau pengamatan ilmiah, hasil pengukuran seringkali tidak sepenuhnya akurat. Perbedaan antara hasil pengukuran dan nilai sebenarnya disebut dengan <b>galat</b> atau <b>kesalahan pengukuran</b>. Galat ini tidak selalu disebabkan oleh ketidaktelitian pengamat, namun juga bisa muncul akibat berbagai faktor yang berkaitan dengan alat ukur, metode yang digunakan, maupun kondisi lingkungan saat pengukuran dilakukan. Memahami penyebab galat sangat penting agar kita dapat meningkatkan ketelitian, mengurangi kesalahan, dan memperoleh hasil yang lebih akurat dalam setiap proses pengukuran.
+    </p>
+    """, unsafe_allow_html=True)
     # Header & Deskripsi Menarik
     st.markdown("""
     <div style='text-align: center; padding: 20px 0;'>
