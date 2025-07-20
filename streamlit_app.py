719077ff19ab1b9d7e7ad0dcf782c94262190ea6
rizavapy <rizava.ankimaka@gmail.com>
2025-07-20 10:50:22 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,7 @@ st.markdown("""
     <style>
     /* Background halaman utama */
     .stApp {
-        background-image: url('https://images.unsplash.com/photo-1660652023138-d464d95b97a0?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDU3fHx8ZW58MHx8fHx8');
+        background-image: url('https://plus.unsplash.com/premium_photo-1661440305059-9397a74f2d7b?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDU2fHx8ZW58MHx8fHx8');
         background-size: cover;
         background-position: center;
         background-attachment: fixed;
